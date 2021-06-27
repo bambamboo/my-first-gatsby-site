fd2f492e6d13aacd9fcc6e5b961298f698716b9b
bambamboo <warm12pm@gmail.com>
2021-06-27 10:02:28 +0700

    Finished Gatsby Tutorial Part 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 // Step 1: Import React
+import { StaticImage } from "gatsby-plugin-image";
 import * as React from "react";
 import Layout from "../components/layout";
 
@@ -6,6 +7,10 @@ const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        alt="A church in the northwest of Iceland"
+        src="../images/church.jpg"
+      />
     </Layout>
   );
 };
